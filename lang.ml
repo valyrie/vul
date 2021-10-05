f7c73b77535329d8a9606bc73d4ecfae029a2dba
Valyrie Jacqueline Heather Autumn <avalyrie@gmail.com>
2021-10-05 04:58:10 +0100

    added license flag

--- lang.ml
+++ lang.ml
@@ -7,7 +7,6 @@ type opt_fn =
     | Apply_String of (string -> unit)
     | Rest
 type opt = {keys: string list; fn: opt_fn; help: string}
-
 let opt_pad (padding: int): string =
     String.init padding (fun _ -> ' ')
 let rec keys_length (keys: string list): int =
@@ -64,11 +63,18 @@ let inc_int (i: int ref) =
 let append_to_list (l: 'a list ref) (x: 'a) =
     l := x :: !l
 
+(* read source files *)
+
+type source_reader =
+    Channel of in_channel
+    | Path of string
+
 (* commandline options *)
 
 let print_help = ref false
 let print_version = ref false
 let print_usage = ref false
+let print_license = ref false
 let verbosity = ref 0
 let (output_paths: string list ref) = ref []
 let (include_paths: string list ref) = ref []
@@ -86,6 +92,7 @@ let opts = [
     {keys = ["-h"; "-?"; "--help"; "--?"]; fn = Apply_Unit (fun () -> set_bool print_help); help = "print this help and exit."};
     {keys = ["-V"; "--version"]; fn = Apply_Unit (fun () -> set_bool print_version); help = "display version and exit."};
     {keys = ["--usage"]; fn = Apply_Unit (fun () -> set_bool print_usage); help = "print usage and exit."};
+    {keys = ["--license"]; fn = Apply_Unit (fun () -> set_bool print_license); help = "print license and exit."};
     {keys = ["-v"; "--verbose"]; fn = Apply_Unit (fun () -> inc_int verbosity); help = "increase verbosity; may be specified multiple times."};
     {keys = ["-o"; "--output"]; fn = Apply_String (append_to_list output_paths); help = "specify an output path; may be specified multiple times."};
     {keys = ["-I"; "--include"]; fn = Apply_String (append_to_list include_paths) ; help = "specify a directory to include; may be specified multiple times."};
@@ -100,4 +107,18 @@ let help = String.concat "\n" [
     license
 ]
 
-let () = print_endline (String.concat " " (parse_opts (List.tl (Array.to_list Sys.argv)) opts)); print_int !verbosity
+let args = parse_opts (List.tl (Array.to_list Sys.argv)) opts
+
+let () = if !print_help then
+    print_endline help
+else if !print_usage || !print_version then
+    begin
+        (if !print_usage then
+            print_endline usage);
+        (if !print_version then
+            print_endline version);
+        (if !print_license then
+            print_endline license)
+    end
+else
+    print_string (String.concat " " args)
