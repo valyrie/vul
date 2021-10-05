4224f8d577616a793b7d7ce950178e81a46aaf9d
Valyrie Jacqueline Heather Autumn <avalyrie@gmail.com>
2021-10-05 11:42:45 +0100

    remove blame type

--- diag.ml
+++ diag.ml
@@ -1,4 +1,3 @@
 (* internal diagnostics *)
 
 type blame_pos = {source: Path.path; off: int}
-type blame = {startat: blame_pos; endat: blame_pos}
--- diag.mli
+++ diag.mli
@@ -1,2 +1,1 @@
 type blame_pos = { source : Path.path; off : int; }
-type blame = { startat : blame_pos; endat : blame_pos; }
