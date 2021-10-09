b6577abbbbdebfd3d39847e1057d8a914e992ffe
Valyrie Jacqueline Heather Autumn <avalyrie@gmail.com>
2021-10-09 05:26:14 +0100

    implemented output and include fns

--- file.ml
+++ file.ml
@@ -18,21 +18,20 @@ exception WrongFileOrDir of string
 
 module Output = struct
   type t = {path: Path.path; channel: out_channel}
-  let open_path (p: Path.path) =
-    if not (path_exists p) then
-      raise (FileNotFound (String.concat "" [(Path.to_string p); ": No such file"]))
-    else
-      if not (is_file p) then
-        raise (WrongFileOrDir (String.concat "" [(Path.to_string p); ": Is not a file"]))
-      else
-        try {path = p; channel = (open_out_bin (Path.to_string p))} with
-          Sys_error _ -> raise (WriteError (String.concat "" [(Path.to_string p); ": Unable to open file for writing"]))
-  (* TODO output fns *)
+  let open_path p =
+    (* TODO check for existence of parent directory *)
+    try {path = p; channel = (open_out_bin (Path.to_string p))} with
+      Sys_error _ -> raise (WriteError (String.concat "" [(Path.to_string p); ": Unable to open file for writing"]))
+  let push_bytes b out =
+    output_bytes out b
+  let close out =
+    try close_out out.channel with
+      Sys_error _ -> raise (WriteError (String.concat "" [(Path.to_string out.path); ": Encountered write error while closing file"]))
 end
 
 module Include = struct
   type t = {path: Path.path}
-  let open_path (p: Path.path) =
+  let make_from_path p =
     if not (path_exists p) then
       raise (FileNotFound (String.concat "" [(Path.to_string p); ": No such directory"]))
     else
@@ -40,19 +39,23 @@ module Include = struct
         raise (WrongFileOrDir (String.concat "" [(Path.to_string p); ": Is not a directory"]))
       else
         {path = p}
-  (* TODO include fns *)
+  let search p inc =
+    if not (path_exists (Path.append inc.path p)) then
+      None
+  else
+      Some (Path.append inc.path p)
 end
 
 module Source = struct
-  type t = {path: Path.path; channel: in_channel; buffer: string; pos: int}
-  let open_path (p: Path.path) =
+  type t = {path: Path.path; channel: in_channel; pos: int}
+  let open_path p =
     if not (path_exists p) then
       raise (FileNotFound (String.concat "" [(Path.to_string p); ": No such file"]))
     else
       if not (is_file p) then
         raise (WrongFileOrDir (String.concat "" [(Path.to_string p); ": Is not a file"]))
       else
-        try {path = p; channel = open_in_bin (Path.to_string p); buffer = ""; pos = 0} with
+        try {path = p; channel = open_in_bin (Path.to_string p); pos = 0} with
           Sys_error _ -> raise (ReadError (String.concat "" [(Path.to_string p); ": Unable to open file for reading"]));
   (* TODO src fns *)
 end
