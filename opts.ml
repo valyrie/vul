e71cef973837de53173be92c92b2562d6fcc6857
Valyrie Jacqueline Heather Autumn <avalyrie@gmail.com>
2021-10-18 06:47:54 +0100

    removed explicit types from opts

--- opts.ml
+++ opts.ml
@@ -5,44 +5,44 @@ type opt_fn =
     | Apply_String of (string -> unit)
     | Rest
 type opt = {keys: string list; fn: opt_fn; help: string}
-let is_switch (s: string): bool =
+let is_switch s =
     (String.get s 0) = '-'
-let opt_pad (padding: int): string =
+let opt_pad padding =
     String.init padding (fun _ -> ' ')
-let rec keys_length (keys: string list): int =
+let rec keys_length keys =
     match keys with
         [] -> 0
         | [k] -> String.length k
         | k :: tail -> (String.length k) + (String.length ", ") + (keys_length tail)
-let rec opt_keys_length (opts: opt list): int =
+let rec opt_keys_length opts =
     match opts with
         [] -> 0
         | opt :: tail -> max (keys_length opt.keys) (opt_keys_length tail)
-let print_opt (klen: int) (opt: opt): string =
+let print_opt klen opt =
     String.concat "" [
         "  ";
         opt_pad (klen - (keys_length opt.keys));
         String.concat ", " opt.keys;
         "  ";
         opt.help]
-let print_opts (opts: opt list): string =
+let print_opts opts =
     let klen = opt_keys_length opts in
         String.concat "\n" (List.map (fun opt -> print_opt klen opt) opts) 
-let rec arg_in_keys (arg: string) (keys: string list): bool =
+let rec arg_in_keys arg keys =
     match keys with
         [] -> false
         | k :: rest -> if arg = k then
             true
         else
             arg_in_keys arg rest
-let rec match_opt (arg: string) (opts: opt list): (opt option) =
+let rec match_opt arg opts =
     match opts with
         [] -> None
         | opt :: rem_opts -> if not (arg_in_keys arg opt.keys) then
             match_opt arg rem_opts
         else
             Some opt
-let rec parse_opts (argv: string list) (opts: opt list): (string list) =
+let rec parse_opts argv opts =
     match argv with
         [] -> []
         | switch :: tail -> match (match_opt switch opts) with
