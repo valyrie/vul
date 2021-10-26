1c1a6e359082cddc7e00854c09d299eb958f84fa
Valyrie Jacqueline Heather Autumn <avalyrie@gmail.com>
2021-10-26 06:22:19 +0100

    lifted more logic into Token.Base

--- compiler/lex.ml
+++ compiler/lex.ml
@@ -29,7 +29,7 @@ module Token = struct
                 | _, Decimal -> Decimal
                 | Octal, _ -> Octal
                 | _, Octal -> Octal
-                | _, _ -> Binary
+                | Binary, Binary -> Binary
         let within a b =
             rebase a b = b
         let of_char_opt c =
@@ -44,7 +44,24 @@ module Token = struct
         let of_char c =
             match of_char_opt c with
                 Some b -> b
-                | None -> raise (Invalid_argument "unknown base specification")
+                | None -> raise (Invalid_argument "unknown base")
+        let of_digit_opt c =
+            match c with
+                '0' | '1' -> Some Binary
+                | '2' | '3' | '4'
+                | '5' | '6' | '7' -> Some Octal
+                | '8' | '9' -> Some Decimal
+                | 'a' | 'A' | 'b' | 'B' | 'c' | 'C'
+                | 'd' | 'D' | 'e' | 'E' | 'f' | 'F' -> Some Hexadecimal
+                | _ -> None
+        let of_digit c =
+            match of_digit_opt c with
+                Some b -> b
+                | None -> raise (Invalid_argument "unknown digit")
+        let is_digit_of c b =
+            within (of_digit c) b
+        let is_digit c =
+            is_digit_of c Hexadecimal
     end
     type t =
         L_parenthesis of from
@@ -77,20 +94,6 @@ let is_implicit_break c =
     is_iws c || c = '\n' || c = '(' || c = ')'
 let is_forbidden_sigil c =
     c = '"' || c = '\'' 
-let is_bin_digit c =
-    c = '0' || c = '1'
-let is_oct_digit c =
-    is_bin_digit c || c = '2' || c = '3' || c = '4' || c = '5' || c = '6' || c = '7'
-let is_dec_digit c =
-    is_oct_digit c || c = '8' || c = '9'
-let is_hex_digit c =
-    is_dec_digit c
-    || c = 'a' || c = 'A'
-    || c = 'b' || c = 'B'
-    || c = 'c' || c = 'C'
-    || c = 'd' || c = 'D'
-    || c = 'e' || c = 'E'
-    || c = 'f' || c = 'F'
 let rec skip_iws l =
     match look l 0 with
         Some c when is_iws c -> advance l 1 |> skip_iws
@@ -158,22 +161,18 @@ let produce_integer p e b s l =
         else
             l |> push (Token.Malformed_integer (from s l.offset l.source))
 let rec lex_int_body p e b s l =
-    match look l 0 with
-        Some c when is_bin_digit c -> advance l 1 |> lex_int_body p (Token.Base.rebase e Token.Base.Binary) (Bytes.cat b (bytes_of_char c)) s
-        | Some c when is_oct_digit c -> advance l 1 |> lex_int_body p (Token.Base.rebase e Token.Base.Octal) (Bytes.cat b (bytes_of_char c)) s
-        | Some c when is_dec_digit c -> advance l 1 |> lex_int_body p (Token.Base.rebase e Token.Base.Decimal) (Bytes.cat b (bytes_of_char c)) s
-        | Some c when is_hex_digit c -> advance l 1 |> lex_int_body p (Token.Base.rebase e Token.Base.Hexadecimal) (Bytes.cat b (bytes_of_char c)) s
-        | Some '_' -> advance l 1 |> lex_int_body p e b s
-        | Some c when is_implicit_break c -> l |> produce_integer p e b s
-        | None -> l |> produce_integer p e b s
-        | Some _ -> l |> lex_mal_int_body s
+    let open Token.Base in
+        match look l 0 with
+            Some c when is_digit c -> advance l 1 |> lex_int_body p (rebase e (of_digit c)) (Bytes.cat b (bytes_of_char c)) s
+            | Some '_' -> advance l 1 |> lex_int_body p e b s
+            | Some c when is_implicit_break c -> l |> produce_integer p e b s
+            | None -> l |> produce_integer p e b s
+            | Some _ -> l |> lex_mal_int_body s
 let lex_prefixed_int_head p s l =
-    match look l 0 with
-        Some c when is_bin_digit c -> l |> lex_int_body p Token.Base.Binary Bytes.empty s
-        | Some c when is_oct_digit c -> l |> lex_int_body p Token.Base.Octal Bytes.empty s
-        | Some c when is_dec_digit c -> l |> lex_int_body p Token.Base.Hexadecimal Bytes.empty s
-        | Some c when is_hex_digit c -> l |> lex_int_body p Token.Base.Hexadecimal Bytes.empty s
-        | _ -> l |> lex_mal_int_body s
+    let open Token.Base in
+        match look l 0 with
+            Some c when is_digit c -> l |> lex_int_body p (of_digit c) Bytes.empty s
+            | _ -> l |> lex_mal_int_body s
 let rec lex_forbidden_ident_body s l =
     match look l 0 with
         Some c when is_implicit_break c -> l |> push (Token.Forbidden_identifier (from s l.offset l.source))
@@ -204,38 +203,39 @@ let rec lex_special_ident_body b s l =
         | None -> l |> push (Token.Unclosed_identifier_body (from s l.offset l.source))
         | Some c -> advance l 1 |> lex_special_ident_body (Bytes.cat b (bytes_of_char c)) s
 let lex_token l =
-    let l = skip_iws l in
-        match look l 0 with
-            (* SIGILS *)
-            | Some '(' -> advance l 1 |> push (Token.L_parenthesis (from l.offset (l.offset + 1) l.source))
-            | Some ')' -> advance l 1 |> push (Token.R_parenthesis (from l.offset (l.offset + 1) l.source))
-            | Some '\'' -> advance l 1 |> push (Token.Quote (from l.offset (l.offset + 1) l.source))
-            | None -> advance l 1 |> push (Token.R_parenthesis (from l.offset l.offset l.source))
-            (* EOL *)
-            | Some '\n' -> begin match look l 1 with
-                Some '\r' -> advance l 2 |> push (Token.Eol (from l.offset (l.offset + 2) l.source))
-                | _ -> advance l 1 |> push (Token.Eol (from l.offset (l.offset + 1) l.source))
-            end
-            (* REMARKS *)
-            | Some '#' -> begin match look l 1 with
-                Some '[' -> advance l 2 |> lex_mlrem_body l.offset
-                | _ -> advance l 1 |> lex_slrem_body
-            end
-            (* STRINGS *)
-            | Some '"' -> advance l 1 |> lex_str_body Bytes.empty l.offset
-            (* INTEGERS *)
-            | Some '0' -> begin match look l 1 with
-                Some c when Token.Base.is_base c -> advance l 2 |> lex_prefixed_int_head (Some (Token.Base.of_char c)) l.offset
-                | _ -> l |> lex_int_body None Token.Base.Decimal Bytes.empty l.offset
-            end
-            | Some c when is_dec_digit c -> l |> lex_int_body (Some Token.Base.Decimal) Token.Base.Decimal Bytes.empty l.offset
-            (* IDENTIFIERS *)
-            | Some 'i' -> begin match look l 1 with
-                Some '"' -> advance l 2 |> lex_special_ident_body Bytes.empty l.offset
-                | _ -> advance l 1 |> lex_ident_body (bytes_of_char 'i') l.offset
-            end
-            | Some '_' -> begin match look l 1 with
-                Some c when is_implicit_break c -> advance l 1 |> push (Token.Wildcard_identifier (from l.offset (l.offset + 1) l.source))
-                | _ -> advance l 1 |> lex_ident_body (bytes_of_char '_') l.offset
-            end
-            | Some c -> advance l 1 |> lex_ident_body (bytes_of_char c) l.offset
+    let open Token in
+        let l = skip_iws l in
+            match look l 0 with
+                (* SIGILS *)
+                | Some '(' -> advance l 1 |> push (Token.L_parenthesis (from l.offset (l.offset + 1) l.source))
+                | Some ')' -> advance l 1 |> push (Token.R_parenthesis (from l.offset (l.offset + 1) l.source))
+                | Some '\'' -> advance l 1 |> push (Token.Quote (from l.offset (l.offset + 1) l.source))
+                | None -> advance l 1 |> push (Token.R_parenthesis (from l.offset l.offset l.source))
+                (* EOL *)
+                | Some '\n' -> begin match look l 1 with
+                    Some '\r' -> advance l 2 |> push (Token.Eol (from l.offset (l.offset + 2) l.source))
+                    | _ -> advance l 1 |> push (Token.Eol (from l.offset (l.offset + 1) l.source))
+                end
+                (* REMARKS *)
+                | Some '#' -> begin match look l 1 with
+                    Some '[' -> advance l 2 |> lex_mlrem_body l.offset
+                    | _ -> advance l 1 |> lex_slrem_body
+                end
+                (* STRINGS *)
+                | Some '"' -> advance l 1 |> lex_str_body Bytes.empty l.offset
+                (* INTEGERS *)
+                | Some '0' -> begin match look l 1 with
+                    Some c when Base.is_base c -> advance l 2 |> lex_prefixed_int_head (Some (Base.of_char c)) l.offset
+                    | _ -> l |> lex_int_body None Decimal Bytes.empty l.offset
+                end
+                | Some c when Base.is_digit_of c Decimal -> l |> lex_int_body (Some Decimal) Decimal Bytes.empty l.offset
+                (* IDENTIFIERS *)
+                | Some 'i' -> begin match look l 1 with
+                    Some '"' -> advance l 2 |> lex_special_ident_body Bytes.empty l.offset
+                    | _ -> advance l 1 |> lex_ident_body (bytes_of_char 'i') l.offset
+                end
+                | Some '_' -> begin match look l 1 with
+                    Some c when is_implicit_break c -> advance l 1 |> push (Token.Wildcard_identifier (from l.offset (l.offset + 1) l.source))
+                    | _ -> advance l 1 |> lex_ident_body (bytes_of_char '_') l.offset
+                end
+                | Some c -> advance l 1 |> lex_ident_body (bytes_of_char c) l.offset
