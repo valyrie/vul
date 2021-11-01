3fbf8a4b160f92a7c02d35fa9ea85b2121e07602
Valyrie Jacqueline Heather Autumn <avalyrie@gmail.com>
2021-11-01 12:24:16 +0100

    Output to stdout / stderr now possible closes #23

--- file/output.mli
+++ file/output.mli
@@ -1,4 +1,7 @@
-type t = { path : Path.t; channel : out_channel; }
+type output_path = Stdout | Stderr | Path of Path.t
+type t = { path : output_path; channel : out_channel; }
+val stdout : t
+val stderr : t
 val open_path : Path.t -> t
 val output_bytes : t -> bytes -> unit
 val close : t -> unit
