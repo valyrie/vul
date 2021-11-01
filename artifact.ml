b5425c2cec4eaf761d4ee33bb25ed79dab384338
Valyrie Jacqueline Heather Autumn <avalyrie@gmail.com>
2021-11-01 12:44:48 +0100

    added close and destroy to artifacts

--- artifact.ml
+++ artifact.ml
@@ -7,6 +7,10 @@ let open_output s =
     match s with
         "-" -> Output.stdout
         | _ -> Output.open_path (Path.of_string s |> Path.normalize_partial)
+let close a =
+    Output.close a.output
+let destroy a =
+    Output.destroy a.output
 let ast_of s w =
     {dst = s; wrt = w; output = open_output s; kind = Ast}
 let ast s =
--- artifact.mli
+++ artifact.mli
@@ -6,5 +6,7 @@ type t = {
   kind : kind;
 }
 val open_output : string -> File.Output.t
+val close : t -> unit
+val destroy : t -> unit
 val ast_of : string -> string option -> t
 val ast : string -> t
