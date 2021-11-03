1d71b0fb7261a56f8eeffee2d4deb69407cf86de
Valyrie Jacqueline Heather Autumn <avalyrie@gmail.com>
2021-11-04 00:44:57 +0100

    added AST module

---
+++ compiler/ast.ml
@@ -0,0 +1,23 @@
+(* AST types *)
+
+module From = struct
+    type t =
+        {offset: int64; stop: int64; source: File.Source.t}
+end
+module rec Expr : sig
+    type t =
+        (* avoid having to wrap in an option type *)
+        None
+        (* cons *)
+        | Cons of {left: Expr.t; right: Expr.t}
+        (* structural whitespace tokens *)
+        | End_of_line of {from: From.t}
+        | Whitespace_break of {from: From.t}
+        (* identifiers *)
+        | Identifier of {bytes: bytes; from: From.t}
+        (* parentheses *)
+        | Left_parenthesis of {from: From.t}
+        | Right_parenthesis of {from: From.t}
+        | Unit of {left: Expr.t; right: Expr.t}
+        | Parentheses of {x: Expr.t; left: Expr.t; right: Expr.t}
+end = Expr
