6c4eb5b019f2d6ccb3c1163b9838ff2f292b3e03
Valyrie Jacqueline Heather Autumn <avalyrie@gmail.com>
2021-11-16 06:57:12 +0000

    can now lex negative bearings close #42

--- compiler/parser.ml
+++ compiler/parser.ml
@@ -27,6 +27,12 @@ let is_digit_or_spacer c =
     match c with
         '_' -> true
         | _ -> is_digit c
+let is_negative c =
+    c = '-'
+let is_sign c =
+    match c with
+        '+' -> true
+        | _ -> is_negative c
 let rec skip_iws p =
     match look p 0 with
         Some c when is_iws c -> advance p 1 |> skip_iws
@@ -44,21 +50,27 @@ let rec lex_malformed_token_body s p: t * Expr.t =
     match look p 0 with
         Some c when not (is_break c) -> advance p 1 |> lex_malformed_token_body s
         | _ -> p, Malformed_token {bytes = Bytestring.of_bytes @@ Source.read_bytes s.source s.offset (p.offset - s.offset); from = make_from s p}
-let rec get_number_body b z =
+let rec get_number_body b s z =
     let open Numbers in
     if Bytes.length b > 0 then
         if Bytes.get b 0 != '_' then
-            get_number_body (Bytes.sub b 1 @@ Bytes.length b - 1) 
+            get_number_body (Bytes.sub b 1 @@ Bytes.length b - 1) s
                 @@ Z.trim
                 @@ Z.add (Z.of_i32 @@ Int32.of_string @@ Bytes.sub_string b 0 1)
                 @@ Z.mul z @@ Z.of_int 10
         else
-            get_number_body (Bytes.sub b 1 @@ Bytes.length b - 1) z
+            get_number_body (Bytes.sub b 1 @@ Bytes.length b - 1) s z
     else
-        z
+        if not s then
+            z
+        else
+            Z.neg z
 let get_number b =
     let open Numbers in
-        get_number_body b Z.zero
+        match Bytes.get b 0 with
+            '-' -> get_number_body (Bytes.sub b 1 @@ Bytes.length b - 1) true Z.zero
+            | '+' -> get_number_body (Bytes.sub b 1 @@ Bytes.length b - 1) false Z.zero
+            | _ -> get_number_body b false Z.zero
 let rec lex_number_body s p =
     match look p 0 with
         Some c when is_digit_or_spacer c -> advance p 1 |> lex_number_body s
@@ -66,13 +78,16 @@ let rec lex_number_body s p =
         | _ -> p, Number {z = get_number @@ Source.read_bytes s.source s.offset (p.offset - s.offset); from = make_from s p}
 let lex_token p: t * Expr.t =
     let p = skip_iws p in
-    match look p 0 with
-        None -> p, None
-        | Some '(' -> advance p 1, Left_parenthesis {from = make_from1 p}
-        | Some ')' -> advance p 1, Right_parenthesis {from = make_from1 p}
-        | Some '\'' -> advance p 1, Quote {from = make_from1 p}
-        | Some c when is_digit c -> advance p 1 |> lex_number_body p
-        | Some _ -> advance p 1 |> lex_ident_body p
+    match look p 0, look p 1 with
+        None, _ -> p, None
+        | Some '(', _ -> advance p 1, Left_parenthesis {from = make_from1 p}
+        | Some ')', _ -> advance p 1, Right_parenthesis {from = make_from1 p}
+        | Some '\'', _ -> advance p 1, Quote {from = make_from1 p}
+        | Some s, Some c when
+            is_sign s
+            && is_digit c -> advance p 2 |> lex_number_body p
+        | Some c, _ when is_digit c -> advance p 1 |> lex_number_body p
+        | Some _, _ -> advance p 1 |> lex_ident_body p
 let push x p =
     {p with v = x :: p.v}
 let rec drop n p =
