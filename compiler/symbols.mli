4cc2cebef30e87bd0bfe96a4952d11c3169c3e3e
Valyrie Jacqueline Heather Autumn <avalyrie@gmail.com>
2021-11-27 12:38:18 +0000

    generated interface for symbols

---
+++ compiler/symbols.mli
@@ -0,0 +1,9 @@
+module Make :
+  functor (K : Map.OrderedType) ->
+    sig
+      type 'a t
+      val empty : 'a t
+      val mem : K.t -> 'a t -> bool
+      val recall : K.t -> 'a t -> 'a option
+      val bind : K.t -> 'a -> 'a t -> 'a t
+    end
