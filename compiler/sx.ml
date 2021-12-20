1d87b49b78cdaa4fad7f142f77c566bb6ced021f
Valyrie Jacqueline Heather Autumn <avalyrie@gmail.com>
2021-12-20 19:46:07 +0000

    added evaluation logic to sx

--- compiler/sx.ml
+++ compiler/sx.ml
@@ -10,6 +10,69 @@ and t =
     | String of Bytestring.t
     | Procedure of proc_fn
     | List of t list
+let unwrap_proc x =
+    match x with
+        Procedure f -> f
+        | _ -> raise @@ Invalid_argument "cannot get fn of non-procedure"
+let hd x =
+    match x with
+        List l -> List.hd l
+        | _ -> raise @@ Invalid_argument "cannot hd non-list"
+let tl x =
+    match x with
+        List l -> List (List.tl l)
+        | _ -> raise @@ Invalid_argument "cannot tl non-list"
+let cons a b =
+    match b with
+        List l -> List (a :: l)
+        | _ -> List [a; b]
+let len x =
+    match x with
+        List l -> List.length l
+        | _ -> raise @@ Invalid_argument "cannot get length of a non-list"
+let map f x =
+    match x with
+        List l -> List (List.map f l)
+        | _ -> raise @@ Invalid_argument "cannot map non-list"
+let iter f x =
+    match x with
+        List l -> List.iter f l
+        | _ -> raise @@ Invalid_argument "cannot iter non-list"
+let fold_left f i x =
+    match x with
+        List l -> List.fold_left f i l
+        | _ -> raise @@ Invalid_argument "cannot fold_left non-list"
+module Eval = struct
+    exception Unbound_name
+    exception Invalid_arguments
+    exception Not_applicable
+    let rec proc_apply x s =
+        if len x > 1 then
+            let proc, s =
+                proc_apply
+                    (hd x)
+                    s in
+            let args = tl x in
+            try (unwrap_proc proc) args s with
+                Invalid_argument _ -> raise Not_applicable
+        else
+            match x with
+                Name n ->
+                    begin match Syms.recall n s with
+                        Some x -> x, s
+                        | _ -> raise Unbound_name
+                    end
+                | _ ->
+                    x, s
+    and proc_sequence x s =
+        match x with
+            List [] ->
+                List [], s
+            | List (hd :: tl) ->
+                let _, s = proc_apply hd, s in
+                proc_sequence (List tl) s
+            | _ -> raise Invalid_arguments
+end
 let rec print x =
     let open Printf in
     match x with
