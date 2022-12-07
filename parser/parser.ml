649eb2be1ac5534396bce4bbb2ea3e054617dd13
Valyrie Jacqueline Heather Autumn <avalyrie@gmail.com>
2022-12-07 12:41:33 +0000

    lex words containing strings w/ subbort for a subset of escape sequences

--- parser/parser.ml
+++ parser/parser.ml
@@ -31,15 +31,16 @@ module Make (R: Reader) = struct
             | Parens of parens
             | Cons of cons
         [@@@ocaml.warning "+30"]
-        let word f = Word {from = f}
-        let lpar f = Lpar {from = f}
-        let rpar f = Rpar {from = f}
-        let empty l r = Empty {left = l; right = r}
-        let orphaned x p = Orphaned {expr = x; prev = p}
-        let malformed f p = Malformed {from = f; prev = p}
-        let parens l x r = Parens {left = l; expr = x; right = r}
-        let cons l r = Cons {left = l; right = r}
     end
+    let word f = Expr.Word {from = f}
+    let lpar f = Expr.Lpar {from = f}
+    let rpar f = Expr.Rpar {from = f}
+    let empty l r = Expr.Empty {left = l; right = r}
+    let orphaned x p = Expr.Orphaned {expr = x; prev = p}
+    let malformed f p = Expr.Malformed {from = f; prev = p}
+    let error e = Expr.Error e
+    let parens l x r = Expr.Parens {left = l; expr = x; right = r}
+    let cons l r = Expr.Cons {left = l; right = r}
     type t = {offset: int; reader: R.t}
     let of_reader r = {offset = 0; reader = r}
     let advance ?(ahead = 1) p = {p with offset = p.offset + ahead}
@@ -51,20 +52,51 @@ module Make (R: Reader) = struct
         c = ' ' || c = '\t' || c = '\n' || c = '\r'
     let is_word_break c =
         is_ws c || c = '(' || c = ')'
+    let is_oct_digit c =
+        c = '0' || c = '1' || c = '2' || c = '3' || c = '4'
+        || c = '5' || c = '6' || c = '7'
+    let is_digit c =
+         is_oct_digit c || c = '8' || c = '9'
+    let is_hex_digit c =
+        is_digit c || c = 'a' || c = 'A' || c = 'b' || c = 'B' || c = 'c' || c = 'C'
+        || c = 'd' || c = 'D' || c = 'e' || c = 'E' || c = 'f' || c = 'F'
     let rec skip_ws p =
         match look_byte p with
               Some c when is_ws c -> skip_ws @@ advance p
             | _ -> p
-    let rec lex_word_body start p =
+    let rec lex_malformed_body start p =
         match look_byte p with
-            Some c when not @@ is_word_break c -> lex_word_body start @@ advance p
-            | _ -> p, Some (Expr.word @@ make_from start p)
+              Some c when is_word_break c -> lex_malformed_body start @@ advance p
+            | _ -> p, Some (error @@ malformed (make_from start p) None)
+    let rec lex_word_body_quoted_escape_hex start p =
+        match look_byte p, look_byte ~ahead:1 p with
+              Some a, Some b when is_hex_digit a && is_hex_digit b -> lex_word_body_quoted start @@ advance ~ahead:2 p
+            | Some a, _ when is_hex_digit a -> lex_word_body_quoted start @@ advance p
+            | _ -> lex_malformed_body start p
+    and lex_word_body_quoted_escape start p =
+        match look_byte p with
+              Some '"' | Some '\\' | Some 'n' | Some 'r' | Some 't' | Some 's'
+            | Some 'b' | Some 'd' | Some 'f' | Some 'v' | Some 'a' | Some 'e' -> lex_word_body_quoted start @@ advance p
+            | Some '0' -> lex_word_body_quoted start @@ advance p
+            | Some 'x' -> lex_word_body_quoted_escape_hex start @@ advance p
+            | Some _ | None -> lex_malformed_body start p
+    and lex_word_body_quoted start p =
+        match look_byte p with
+              Some '"' -> lex_word_body start @@ advance p
+            | Some '\\' -> lex_word_body_quoted start @@ advance ~ahead:2 p
+            | Some _ -> lex_word_body_quoted start @@ advance p
+            | None -> lex_malformed_body start p
+    and lex_word_body start p =
+        match look_byte p with
+              Some '"' -> lex_word_body_quoted start @@ advance p
+            | Some c when not @@ is_word_break c -> lex_word_body start @@ advance p
+            | _ -> p, Some (word @@ make_from start p)
     let lex p =
         let p = skip_ws p in
         match look_byte p with
               None -> p, None
-            | Some '(' -> advance p, Some (Expr.lpar @@ make_from p.offset @@ advance p)
-            | Some ')' -> advance p, Some (Expr.rpar @@ make_from p.offset @@ advance p)
+            | Some '(' -> advance p, Some (lpar @@ make_from p.offset @@ advance p)
+            | Some ')' -> advance p, Some (rpar @@ make_from p.offset @@ advance p)
             | Some _ -> lex_word_body p.offset p
     (* TODO *)
 end
