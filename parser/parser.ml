ba351b63a03f90afc8870934118a4d0de4873b56
Valyrie Jacqueline Heather Autumn <avalyrie@gmail.com>
2022-12-07 10:55:38 +0000

    do not differentiate word tokens yet

--- parser/parser.ml
+++ parser/parser.ml
@@ -11,16 +11,10 @@ module Make (R: Reader) = struct
     module Expr = struct
         [@@@ocaml.warning "-30"]
         type from = {start: int; stop: int; reader: R.t}
-        type ident = {from: from}
+        type word = {from: from}
         type lpar = {from: from}
         type rpar = {from: from}
         type empty = {left: lpar; right: rpar}
-        type num = {from: from}
-        type str = {from: from}
-        type lit =
-              Empty of empty
-            | Num of num
-            | Str of str
         type orphaned = {expr: t; prev: error option}
         and malformed = {from: from; prev: error option}
         and error =
@@ -29,16 +23,14 @@ module Make (R: Reader) = struct
         and parens = {left: lpar; expr: t; right: rpar}
         and cons = {left: t; right: t option}
         and t =
-              Ident of ident
+              Word of word
             | Lpar of lpar
             | Rpar of rpar
             | Empty of empty
-            | Lit of lit
             | Error of error
             | Parens of parens
             | Cons of cons
         [@@@ocaml.warning "+30"]
     end
-    type t = {pos: int; reader: R.t}
     (* TODO *)
 end
